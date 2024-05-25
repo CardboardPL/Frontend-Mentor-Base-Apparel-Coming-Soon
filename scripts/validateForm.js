8679779418f2f7a89e7c634bb620504e695cd1c1
CardboardPL <mrplwaschilling@gmail.com>
2024-05-25 21:40:33 +0800

    Updated: validateForm.js
    In this commit:
    - Updated validateForm.js: Correctly select the emailInput element.

--- scripts/validateForm.js
+++ scripts/validateForm.js
@@ -1,5 +1,5 @@
 const form = document.querySelector('.hero__form')
-const emailInputElem = document.querySelector('#email');
+const emailInputElem = document.querySelector('#emailInput');
 
 form.onsubmit = (event) => {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
