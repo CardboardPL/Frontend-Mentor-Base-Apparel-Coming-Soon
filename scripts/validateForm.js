0f4e4ec4bb3912524ca690eda8ad5dbd735c1122
CardboardPL <mrplwaschilling@gmail.com>
2024-05-26 21:19:38 +0800

    Updated: index.html and validateForm.js
    In this commit:
    - Updated index.html:
      - Add a wrapper element for the form elements.
      - Add an error message container.
      - Add some utility classes for future styling.
      - Link the utility.css file.
    - Updated validateForm.js:
      - Add a variable to select the error message container.
      - Change the onsubmit property to instead use an event listener.
      - Abstracted the code by dividing the functionality into multiple functions.

--- scripts/validateForm.js
+++ scripts/validateForm.js
@@ -1,9 +1,32 @@
 const form = document.querySelector('.hero__form')
 const emailInputElem = document.querySelector('#emailInput');
+const errorMsgContainer = form.querySelector('.hero__form-error-message');
 
-form.onsubmit = (event) => {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!regex.test(emailInputElem.value)) {
-    event.preventDefault();
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const isValidEmail = validateEmail(emailInputElem.value.trim(""));
+  
+  if (!isValidEmail) {
+    showError();
+  } else {
+    hideError();
+    form.reset();
   }
+})
+
+function validateEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
+
+function showError() {
+  emailInputElem.classList.add('hero__input--invalid');
+  errorMsgContainer.classList.remove('hero__form-error-message--hidden');
+  errorMsgContainer.setAttribute('aria-hidden', 'false');
+}
+
+function hideError() {
+  emailInputElem.classList.remove('hero__input--invalid');
+  errorMsgContainer.classList.add('hero__form-error-message--hidden');
+  errorMsgContainer.setAttribute('aria-hidden', 'true');
 }
